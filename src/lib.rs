124e169c4f5b106367f99b262caab7a79d4a720b
Félix Saparelli <felix@passcod.name>
2021-07-20 06:40:15 +1200

    Remove unstable? rustdoc lint

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! On Unix, the [`UnixChildExt`] trait additionally adds support for sending signals to processes
 //! and process groups (it’s implemented on _both_ this crate’s [`GroupChild`] and std’s [`Child`]).
 
-#![warn(missing_docs, rustdoc::missing_crate_level_docs)]
+#![warn(missing_docs)]
 
 mod child;
 pub mod stdlib;
