73e33cefd613c8da8b60d1ac19f242b9b8f0eb0c
Félix Saparelli <felix@passcod.name>
2021-07-26 04:59:38 +1200

    Extract more common windows parts into winres

--- src/stdlib/windows.rs
+++ src/stdlib/windows.rs
@@ -1,107 +1,19 @@
 use std::{
-	convert::TryInto,
 	io::Result,
-	mem,
 	os::windows::{io::AsRawHandle, process::CommandExt},
 	process::Command,
-	ptr,
 };
-use winapi::{
-	shared::minwindef::LPVOID,
-	um::{
-		handleapi::{CloseHandle, INVALID_HANDLE_VALUE},
-		ioapiset::CreateIoCompletionPort,
-		jobapi2::{AssignProcessToJobObject, CreateJobObjectW, SetInformationJobObject},
-		processthreadsapi::{GetProcessId, OpenThread, ResumeThread},
-		tlhelp32::{
-			CreateToolhelp32Snapshot, Thread32First, Thread32Next, TH32CS_SNAPTHREAD, THREADENTRY32,
-		},
-		winbase::CREATE_SUSPENDED,
-		winnt::{
-			JobObjectAssociateCompletionPortInformation, JobObjectExtendedLimitInformation, HANDLE,
-			JOBOBJECT_ASSOCIATE_COMPLETION_PORT, JOBOBJECT_EXTENDED_LIMIT_INFORMATION,
-			JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE,
-		},
-	},
-};
+use winapi::um::winbase::CREATE_SUSPENDED;
 
 use crate::{winres::*, CommandGroup, GroupChild};
 
 impl CommandGroup for Command {
 	fn group_spawn(&mut self) -> Result<GroupChild> {
-		let job = res_null(unsafe { CreateJobObjectW(ptr::null_mut(), ptr::null()) })?;
-
-		let completion_port = res_null(unsafe {
-			CreateIoCompletionPort(INVALID_HANDLE_VALUE, ptr::null_mut(), 0, 1)
-		})?;
-
-		let mut associate_completion = JOBOBJECT_ASSOCIATE_COMPLETION_PORT {
-			CompletionKey: job,
-			CompletionPort: completion_port,
-		};
-
-		res_bool(unsafe {
-			SetInformationJobObject(
-				job,
-				JobObjectAssociateCompletionPortInformation,
-				&mut associate_completion as *mut _ as LPVOID,
-				mem::size_of_val(&associate_completion)
-					.try_into()
-					.expect("cannot safely cast to DWORD"),
-			)
-		})?;
-
-		let mut info = JOBOBJECT_EXTENDED_LIMIT_INFORMATION::default();
-		info.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
-		res_bool(unsafe {
-			SetInformationJobObject(
-				job,
-				JobObjectExtendedLimitInformation,
-				&mut info as *mut _ as LPVOID,
-				mem::size_of_val(&info)
-					.try_into()
-					.expect("cannot safely cast to DWORD"),
-			)
-		})?;
-
+		let (job, completion_port) = job_object()?;
 		self.creation_flags(CREATE_SUSPENDED);
-
 		let child = self.spawn()?;
-
-		let handle = child.as_raw_handle() as _;
-		res_bool(unsafe { AssignProcessToJobObject(job, handle) })?;
-		resume_threads(handle)?;
+		assign_child(child.as_raw_handle(), job)?;
 
 		Ok(GroupChild::new(child, job, completion_port))
 	}
-}
-
-// This is pretty terrible, but it's either this or we re-implement all of Rust's std::process just
-// to get at PROCESS_INFORMATION!
-fn resume_threads(child_process: HANDLE) -> Result<()> {
-	let child_id = unsafe { GetProcessId(child_process) };
-
-	let h = res_null(unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0) })?;
-	let mut entry = THREADENTRY32 {
-		dwSize: 28,
-		cntUsage: 0,
-		th32ThreadID: 0,
-		th32OwnerProcessID: 0,
-		tpBasePri: 0,
-		tpDeltaPri: 0,
-		dwFlags: 0,
-	};
-
-	let mut res = res_bool(unsafe { Thread32First(h, &mut entry) });
-	while res.is_ok() {
-		if entry.th32OwnerProcessID == child_id {
-			let thread_handle = res_null(unsafe { OpenThread(0x0002, 0, entry.th32ThreadID) })?;
-			res_neg(unsafe { ResumeThread(thread_handle) })?;
-			res_bool(unsafe { CloseHandle(thread_handle) })?;
-		}
-
-		res = res_bool(unsafe { Thread32Next(h, &mut entry) });
-	}
-
-	res_bool(unsafe { CloseHandle(h) })
 }
--- src/winres.rs
+++ src/winres.rs
@@ -1,7 +1,26 @@
-use std::io::{Error, Result};
+use std::{
+	convert::TryInto,
+	io::{Error, Result},
+	mem,
+	os::windows::io::RawHandle,
+	ptr,
+};
 use winapi::{
-	shared::minwindef::{BOOL, DWORD},
-	um::winnt::HANDLE,
+	shared::minwindef::{BOOL, DWORD, LPVOID},
+	um::{
+		handleapi::{CloseHandle, INVALID_HANDLE_VALUE},
+		ioapiset::CreateIoCompletionPort,
+		jobapi2::{AssignProcessToJobObject, CreateJobObjectW, SetInformationJobObject},
+		processthreadsapi::{GetProcessId, OpenThread, ResumeThread},
+		tlhelp32::{
+			CreateToolhelp32Snapshot, Thread32First, Thread32Next, TH32CS_SNAPTHREAD, THREADENTRY32,
+		},
+		winnt::{
+			JobObjectAssociateCompletionPortInformation, JobObjectExtendedLimitInformation, HANDLE,
+			JOBOBJECT_ASSOCIATE_COMPLETION_PORT, JOBOBJECT_EXTENDED_LIMIT_INFORMATION,
+			JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE,
+		},
+	},
 };
 
 pub(crate) fn res_null(handle: HANDLE) -> Result<HANDLE> {
@@ -26,4 +45,79 @@ pub(crate) fn res_neg(ret: DWORD) -> Result<DWORD> {
 	} else {
 		Ok(ret)
 	}
+}
+
+pub(crate) fn job_object() -> Result<(HANDLE, HANDLE)> {
+	let job = res_null(unsafe { CreateJobObjectW(ptr::null_mut(), ptr::null()) })?;
+
+	let completion_port =
+		res_null(unsafe { CreateIoCompletionPort(INVALID_HANDLE_VALUE, ptr::null_mut(), 0, 1) })?;
+
+	let mut associate_completion = JOBOBJECT_ASSOCIATE_COMPLETION_PORT {
+		CompletionKey: job,
+		CompletionPort: completion_port,
+	};
+
+	res_bool(unsafe {
+		SetInformationJobObject(
+			job,
+			JobObjectAssociateCompletionPortInformation,
+			&mut associate_completion as *mut _ as LPVOID,
+			mem::size_of_val(&associate_completion)
+				.try_into()
+				.expect("cannot safely cast to DWORD"),
+		)
+	})?;
+
+	let mut info = JOBOBJECT_EXTENDED_LIMIT_INFORMATION::default();
+	info.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
+	res_bool(unsafe {
+		SetInformationJobObject(
+			job,
+			JobObjectExtendedLimitInformation,
+			&mut info as *mut _ as LPVOID,
+			mem::size_of_val(&info)
+				.try_into()
+				.expect("cannot safely cast to DWORD"),
+		)
+	})?;
+
+	Ok((job, completion_port))
+}
+
+// This is pretty terrible, but it's either this or we re-implement all of Rust's std::process just
+// to get at PROCESS_INFORMATION!
+fn resume_threads(child_process: HANDLE) -> Result<()> {
+	let child_id = unsafe { GetProcessId(child_process) };
+
+	let h = res_null(unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0) })?;
+	let mut entry = THREADENTRY32 {
+		dwSize: 28,
+		cntUsage: 0,
+		th32ThreadID: 0,
+		th32OwnerProcessID: 0,
+		tpBasePri: 0,
+		tpDeltaPri: 0,
+		dwFlags: 0,
+	};
+
+	let mut res = res_bool(unsafe { Thread32First(h, &mut entry) });
+	while res.is_ok() {
+		if entry.th32OwnerProcessID == child_id {
+			let thread_handle = res_null(unsafe { OpenThread(0x0002, 0, entry.th32ThreadID) })?;
+			res_neg(unsafe { ResumeThread(thread_handle) })?;
+			res_bool(unsafe { CloseHandle(thread_handle) })?;
+		}
+
+		res = res_bool(unsafe { Thread32Next(h, &mut entry) });
+	}
+
+	res_bool(unsafe { CloseHandle(h) })
+}
+
+pub(crate) fn assign_child(handle: RawHandle, job: HANDLE) -> Result<()> {
+	let handle = handle as _;
+	res_bool(unsafe { AssignProcessToJobObject(job, handle) })?;
+	resume_threads(handle)?;
+	Ok(())
 }
