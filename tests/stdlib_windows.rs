d16879f6c8d18157a8e1ce11be9312e3a400c61a
Félix Saparelli <felix@passcod.name>
2021-08-26 04:22:48 +1200

    Increase sleep for std try_wait tests

--- tests/stdlib_windows.rs
+++ tests/stdlib_windows.rs
@@ -153,7 +153,7 @@ fn try_wait_after_die_normal() -> Result<()> {
 		.arg("/C")
 		.arg("echo hello")
 		.spawn()?;
-	sleep(DIE_TIME);
+	sleep(DIE_TIME * 10);
 
 	let status = child.try_wait()?;
 	assert!(status.is_some());
@@ -168,7 +168,7 @@ fn try_wait_after_die_group() -> Result<()> {
 		.arg("/C")
 		.arg("echo hello")
 		.group_spawn()?;
-	sleep(DIE_TIME);
+	sleep(DIE_TIME * 10);
 
 	let status = child.try_wait()?;
 	assert!(status.is_some());
